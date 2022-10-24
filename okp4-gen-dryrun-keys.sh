fd26f2de1b68ba05109da2f37b91e35aaf452512
Peter <pfilia@mail.ru>
2022-10-24 13:51:46 +0300

    refactor & add script for dryrun generate keys

---
+++ okp4-gen-dryrun-keys.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PREFIX=${1} 	# Prefix for new wallets
+WALLETSNUM=${2} # Number of wallets generated
+OKP4BIN=okp4d
+
+FILENAME=${PREFIX}.keys
+
+LIST="[]"
+
+for (( i=1; i<=$WALLETSNUM; i++ ))
+do
+    NEW_KEY=$($OKP4BIN keys add ${PREFIX}${i} --keyring-backend=test --dry-run --output json)
+    LIST=$(echo $LIST | jq  ". += [$NEW_KEY]")
+done
+
+echo $LIST | jq > $FILENAME
--- okp4-gen-keys.sh
+++ okp4-gen-keys.sh
@@ -4,38 +4,24 @@ PREFIX=${1} 	# Prefix for new wallets
 PASSWORD=${2} 	# Current wallet Password
 WALLETSNUM=${3} # Number of wallets generated
 KEYRING_BACKEND=${4}
-
-	if [ -z "$KEYRING_BACKEND" ]
-	then
- 	   KEYRING_BACKEND="file"
-	fi
+OKP4BIN=okp4d
 
 FILENAME=${PREFIX}.keys
 
+if [ -z "$KEYRING_BACKEND" ]
+then
+   KEYRING_BACKEND="file"
+fi
+
 echo "Будет сгенерировано $WALLETSNUM кошельков. Результат сохранится в файле $FILENAME"
-
-
-OKP4BIN=okp4d
-KEYRING_BACKEND=file #
 
 LIST="[]"
 
 for (( i=1; i<=$WALLETSNUM; i++ ))
 do
-
-    NEW_KEY=$(echo ${PASSWORD} | $OKP4BIN keys add ${PREFIX}${i} --keyring-backend=${KEYRING_BACKEND} --output json)
-    
-    name=${PREFIX}${i} #$(echo $NEW_KEY | jq -r '.name')
-    address=$(echo $NEW_KEY | jq -r '.address')
-    mnemonic=$(echo $NEW_KEY | jq -r '.mnemonic')
-    pubkey=$(echo $NEW_KEY | jq '.pubkey')
-
-	echo "Кошелек: $name. Публичный ключ: $address"     
-    
-    #LIST=$(echo $LIST | jq  ". += [{\"name\":\"${name}\", \"address\":\"${address}\", \"mnemonic\":\"${mnemonic}\", \"pubkey\":${pubkey}}]")
-    LIST=$(echo $LIST | jq  ". += [{\"name\":\"${name}\", \"address\":\"${address}\", \"mnemonic\":\"${mnemonic}\"}]")
+	echo "Кошелек $i из $WALLETSNUM"
+    NEW_KEY=$($OKP4BIN keys add ${PREFIX}${i} --keyring-backend=${KEYRING_BACKEND} --output json)
+    LIST=$(echo $LIST | jq  ". += [$NEW_KEY]")
 done
 
 echo $LIST | jq > $FILENAME
-
-
