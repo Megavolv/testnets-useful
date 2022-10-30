36047c8a3463c2f1b6978768306f4d22fba1f38d
Peter <pfilia@mail.ru>
2022-10-30 20:00:35 +0300

    solana-alive-checker: clarified trigger condition and updated readme

--- solana-alive-checker.sh
+++ solana-alive-checker.sh
@@ -5,8 +5,9 @@ name=${1} // Название сервера
 while :
 do
     status=$(service solanad status  | grep dead | wc -l)
+    restart=$(service solanad status  | grep restart | wc -l)
 
-    if ((status >= 1)); then
+    if ((status >= 1 || restart > 1)); then
         ./pigeon --text "$name: solana service is dead;" --bot fl
 	sleep 5m
     fi
